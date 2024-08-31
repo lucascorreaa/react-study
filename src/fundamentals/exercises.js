dbb4ed8d1532f0e0789c32b055775d6cedff5b17
lucascorreaa <lucas.lca14@gmail.com>
2024-08-31 13:48:39 -0300

    feat: added switch case

--- src/fundamentals/exercises.js
+++ src/fundamentals/exercises.js
@@ -78,3 +78,46 @@ function verificaValor(valor) {
 // verificaValor(10)
 // verificaValor(-20)
 // verificaValor(0)
+
+function diaDaSemana(dia) {
+   switch (dia) {
+    case 1: return console.log("Domingo")
+    case 2: return console.log("Segunda")
+    case 3: return console.log("Terça")
+    case 4: return console.log("Quarta")
+    case 5: return console.log("Quinta")
+    case 6: return console.log("Sexta")
+    case 7: return console.log("Sábado")
+    default: return console.log("Dia inválido")
+   }
+}
+// diaDaSemana(0)
+
+function handleMonth(mes) {
+  switch (mes) {
+    case 1: return console.log('Jan')
+    case 2: return console.log('Fev')
+    case 3: return console.log('Mar')
+    case 4: return console.log('Abr')
+    case 5: return console.log('Mai')
+    case 6: return console.log('Jun')
+    case 7: return console.log('Jul')
+    case 8: return console.log('Agost')
+    case 9: return console.log('Set')
+    case 10: return console.log('Out')
+    case 11: return console.log('Nov')
+    case 12: return console.log('Dec')
+    default: return console.log('Mês inválido')
+  }
+}
+// handleMonth(30)
+
+function handleDev(cargo) {
+  switch (cargo) {
+    case 'inciante': return console.log('Vai estudar!')
+    case 'intermediario': return console.log('Vai estudar o foco das vagas!')
+    case 'avancado': return console.log('Vai aprimorar o conhecimento!')
+    default: return console.log('Cargo inexistente')
+  }
+}
+handleDev('a')
