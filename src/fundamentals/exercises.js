e4e3da3e10b09530811ad47b0edfe1cddcccdc2b
lucascorreaa <lucas.lca14@gmail.com>
2024-08-31 11:50:26 -0300

    feat: created if else case

--- src/fundamentals/exercises.js
+++ src/fundamentals/exercises.js
@@ -36,3 +36,45 @@ function validaFidelidade(clienteFidelidade) {
   )
 }
 // validaFidelidade(true)
+
+function validaIdadeIf(idade) {
+  if (idade >= 18) {
+    return console.log("É maior de idade")
+  } else {
+    return console.log("É menor de idade")
+  }
+}
+// validaIdadeIf(19)
+
+function validaMedia(nota) {
+  if (nota >= 7) {
+    return console.log('Aprovado')
+  } else if (nota >= 5 && nota <= 6) {
+    return console.log('Está de recuperação')
+  } else {
+    return console.log('reprovado')
+  }
+}
+// validaMedia(9)
+
+function validaNota(nota) {
+  if (nota >= 60) {
+    return console.log("aprovado")
+  } else {
+    return console.log("reprovado")
+  }
+}
+// validaNota(3)
+
+function verificaValor(valor) {
+  if (valor > 0) {
+    return console.log('positivo')
+  } else if (valor < 0) {
+    return console.log('negativo') 
+  } else {
+    return console.log('zero')
+  }
+}
+// verificaValor(10)
+// verificaValor(-20)
+// verificaValor(0)
