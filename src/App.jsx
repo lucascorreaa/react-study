48b922a963b30b770f1f4fdf3b07771104178d91
lucascorreaa <lucas.lca14@gmail.com>
2024-09-14 09:51:28 -0300

    fix: clean project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import Count from './class/hooks/useState/count';
-=======
-import Hero from './components/hero/'
->>>>>>> 192bc14458137ca97fe8c779aa82a68e98ba4950
 
 export default function App() {
   return (
     <div className="app-container">
-<<<<<<< HEAD
       <Count />
-=======
-      <Hero alignment="right" />
->>>>>>> 192bc14458137ca97fe8c779aa82a68e98ba4950
     </div>
   );
 }
