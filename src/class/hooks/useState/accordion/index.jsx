67785465d30bdc3f9e559c9e1179f95f20e6c415
lucascorreaa <lucas.lca14@gmail.com>
2024-09-14 11:26:50 -0300

    style: refactor component style

--- src/class/hooks/useState/accordion/index.jsx
+++ src/class/hooks/useState/accordion/index.jsx
@@ -3,14 +3,18 @@ import { ArrowDown, ArrowUp } from "lucide-react";
 import "./styles.css";
 
 export default function Accordion() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="accordion-container">
       <div className="accordion-title">
         <p>Novidades</p>
         {/* se estiver aberto é um ícone, caso fechado outro */}
         <button onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? <ArrowUp /> : <ArrowDown />}
+          {isOpen ? (
+            <ArrowUp color="#272932" size={18} />
+          ) : (
+            <ArrowDown color="#272932" size={18} />
+          )}
         </button>
       </div>
       {/* e-comercial pq eu n mostro nada caso estiver false */}
